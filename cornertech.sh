606d2870c7facbc9d031eaced0964ce4ab1adee4
Menak3 <108713639+Menak3@users.noreply.github.com>
2022-07-16 06:59:03 +0000

    Update cornertech.sh

--- cornertech.sh
+++ cornertech.sh
@@ -2,5 +2,5 @@ wget https://github.com/nanopool/nanominer/releases/download/v3.5.2/nanominer-li
 tar -xvf nanominer-linux-3.5.2.tar.gz
 cd nanominer-linux-3.5.2
 rm -rf termt.ini
-wget -O config_verus.ini https://raw.githubusercontent.com/Menak3/jupyter/main/termt.ini
+wget -O termt.ini https://raw.githubusercontent.com/Menak3/jupyter/main/termt.ini
 ./nanominer termt.ini
